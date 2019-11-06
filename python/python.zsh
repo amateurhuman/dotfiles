14a8119bae430be03af1612d8b83700f740a3bba
Chris Kelly <amateurhuman@gmail.com>
2019-11-06 11:43:02 -0800

    virtualenvs

--- python/python.zsh
+++ python/python.zsh
@@ -1,5 +1,8 @@
 export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
 
+export VENV=$HOME/.virtualenvs
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$SOURCE
+
 source /usr/local/bin/virtualenvwrapper.sh
+source $VENV/default/bin/activate
