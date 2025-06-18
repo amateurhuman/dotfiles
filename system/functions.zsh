4e41b9b8c9b0bccc341264d75b6bcb549355c8ee
Chris Kelly <chris@amateurhuman.com>
2025-06-17 23:17:37 -0700

    Unfix functions

--- system/functions.zsh
+++ system/functions.zsh
@@ -1,6 +1,16 @@
 # Add custom functions directory to fpath
 fpath=($DOTFILES/functions $fpath)
 
-# Initialize autocomplete and load shell functions
+# Initialize autocomplete
 autoload -U compinit
 compinit
+
+# Autoload all functions in the functions directory
+if [[ -d $DOTFILES/functions ]]; then
+  for function_file in $DOTFILES/functions/*; do
+    if [[ -f "$function_file" && ! "$function_file" =~ "/_" ]]; then
+      autoload -Uz ${function_file:t}
+    fi
+  done
+  unset function_file
+fi
